15004dee100596e558031cc9b62553a5856a08f0
Brandon Anderson <brandonanderson@blueskychurch.com>
2020-04-29 12:55:48 -0700

    added rendering of flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,17 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "logged in successfully"
+      flash[:success] = "Welcome back! You have logged in successfully"
       redirect_to root_path
     else
-      flash.now[:alert] = "There was something wrong with your login details"
+      flash.now[:error] = "Hmmmm... There was something wrong with your login details"
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have successfully logged out"
+    flash[:success] = "You have successfully logged out; come back soon!"
     redirect_to login_path
   end
 
