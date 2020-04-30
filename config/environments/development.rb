ce8a891bca6b38fe45a4c00c0368c37dd47159fe
Brandon Anderson <brandonanderson@blueskychurch.com>
2020-04-30 13:15:44 -0700

    moved heroku path from development to production l

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_cable.allowed_request_origins = ['https://anderson-message-me.herokuapp.com/']
 end
